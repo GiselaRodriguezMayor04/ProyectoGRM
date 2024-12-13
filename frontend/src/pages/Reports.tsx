192373d3c5d0d7d7e3c566966ac0841e1882bbe9
“GiselaRodriguezMayor04” <giselarodriguezmayor2004@gmail.com>
2024-12-13 10:00:52 +0000

    examen REC ut3

--- frontend/src/pages/Reports.tsx
+++ frontend/src/pages/Reports.tsx
@@ -1,63 +1,57 @@
 import { useEffect, useState } from 'react';
 import Menu from '../components/Menu';
-import Button from '@mui/material/Button'
-import Grid from '@mui/material/Grid'
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
 import InformeColeccion from '../components/InformeColeccion';
 import InformeDevaluacion from '../components/InformeDevaluacion';
 
 function Reports() {
-  const [tableDataC, setTableDataC] = useState([])
-  const [pic, setPicado] = useState(false);
+  const [tableDataC, setTableDataC] = useState([]);
+  const [tableDataD, setTableDataD] = useState([]);
+  const [selectedReport, setSelectedReport] = useState<string | null>(null);
 
   const fetchItems = () => {
     fetch("http://localhost:3030/getItems")
       .then((response) => response.json())
       .then((response) => {
-        console.log("Datos recibidos del backend:", response.data);
         setTableDataC(response.data);
-        });
+      });
   };
 
-  const fetchdeval = () => {
+  const fetchDeval = () => {
     fetch("http://localhost:3030/getdeval")
-    .then((response) => response.json())
-    .then((response) => {
-      console.log("Datos recibidos del backend:", response.data);
-      setTableDataC(response.data);
-  });
-};
-
-
+      .then((response) => response.json())
+      .then((response) => {
+        setTableDataD(response.data);
+      });
+  };
 
   useEffect(() => {
     fetchItems();
+    fetchDeval();
   }, []);
 
-  useEffect(() => {
-    fetchdeval();
-  }, []);
-
-  const picado = () => {
-    setPicado(true);
+  const showReport = (reportType: string) => {
+    setSelectedReport(reportType);
   };
 
   return (
     <>
-        <Menu/>
-        <Grid container spacing={2} sx={{margin:"0 auto", width:"200px", marginTop:"20px"}}>
-          <Button id='but' onClick={picado} type="submit" variant='contained' fullWidth>INFORME COLECCION</Button>
-        </Grid>
-        {(pic === true) && (
-          <InformeColeccion data={tableDataC}/>
-        )} 
-        <Grid container spacing={2} sx={{margin:"0 auto", width:"200px", marginTop:"20px"}}>
-          <Button id='but' onClick={picado} type="submit" variant='contained' fullWidth>INFORME DEVALUACION</Button>
-        </Grid>
-        {(pic === true) && (
-          <InformeDevaluacion data={tableDataC}/>
-        )} 
-    </>
+      <Menu />
+      <Grid container spacing={2} sx={{ margin: "0 auto", width: "200px", marginTop: "20px" }}>
+        <Button id='but' onClick={() => showReport('coleccion')} type="submit" variant='contained' fullWidth>INFORME COLECCION</Button>
+      </Grid>
+      {selectedReport === 'coleccion' && (
+        <InformeColeccion data={tableDataC} />
+      )}
 
+      <Grid container spacing={2} sx={{ margin: "0 auto", width: "200px", marginTop: "20px" }}>
+        <Button id='but' onClick={() => showReport('devaluacion')} type="submit" variant='contained' fullWidth>INFORME DEVALUACION</Button>
+      </Grid>
+      {selectedReport === 'devaluacion' && (
+        <InformeDevaluacion data={tableDataD} />
+      )}
+    </>
   );
 }
 
